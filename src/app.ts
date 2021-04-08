801fa517a753bdccf582f7bc81db17ce46a6e06c
Akbar Shaikh <akbarshaikh.2020@gmail.com>
2021-04-08 09:52:16 +0530

    getGameId route added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 const port = 4001;
 
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send('First route');
+})
+
+function isUniqueGameId(gameId):boolean {
+    return true;
+}
+
+app.get('/getUniqueGameId',(req,res)=>{
+
+    let gameId = Math.random().toString(36).slice(-10);
+
+    while(!isUniqueGameId(gameId)) {
+      gameId = Math.random().toString(36).slice(-10);
+    }
+
+    res.send({
+        gameId,
+        status: true,
+    });
 })
 
 app.listen(port,()=>{
