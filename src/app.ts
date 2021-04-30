3538b2db86e50c31f4e619801a061edb46e132e7
Akbar Shaikh <akbarshaikh.2020@gmail.com>
2021-04-30 09:08:12 +0530

    movement stored in db

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,19 @@
 import express from "express";
 import cors from "cors";
 import { ChessGame } from "./database";
+var mongoose = require('mongoose');
 
 const app = express();
-import http from 'http';
+import http from "http";
 const server = new http.Server(app);
-import {Server} from 'socket.io';
-
-
+import { Server } from "socket.io";
 
 const port = 4001;
 
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
@@ -134,33 +133,127 @@ server.listen(port, () => {
   console.log(`Server runnig on PORT ${port}`);
 });
 
-var io = new Server(server,{
+var io = new Server(server, {
   cors: {
-    origin: '*',
-  }
+    origin: "*",
+  },
 });
 
 //Whenever someone connects this gets executed
-io.on('connection', function(socket) {
-  console.log('A user connected');
+io.on("connection", function (socket) {
+  console.log("A user connected");
 
-  socket.on('currentState', (data) => {
-    console.log('data received', data);
+  socket.on("currentState", (data) => {
+    console.log("data received", data);
 
-    ChessGame.find({ gameId: data['gameId']}, function (err, docs) {
+    ChessGame.find({ gameId: data["gameId"] }, function (err, docs) {
       if (err) {
-        socket.emit('currentStateResp', [])
+        socket.emit("currentStateResp", []);
       } else {
-        socket.emit('currentStateResp', docs[0]['state']);
+        if (docs.length >= 1) {
+          socket.emit("currentStateResp", docs[0]["state"]);
+        } else {
+          socket.emit("currentStateResp", []);
+        }
       }
-
     });
+  });
 
-    
-  })
+  socket.on("move", (data) => {
+    console.log("moved", JSON.stringify(data, null, 2));
+
+    ChessGame.updateOne(
+      { gameId: data["gameId"] },
+      {
+        $push: {
+          movements: {
+            name: data["piece"]["name"],
+            color: data["piece"]["color"],
+            start: {
+              row: data["previousPosition"]["row"],
+              column: data["previousPosition"]["column"],
+            },
+            end: {
+              row: data["piece"]["position"]["row"],
+              column: data["piece"]["position"]["column"],
+            },
+          },
+        },
+      },
+      function (err, doc) {
+        if (err) {
+          console.log("failed to update record", err);
+        }
+        console.log("movement saved");
+
+        const where = {
+          gameId: data["gameId"],
+          "state._id": mongoose.Types.ObjectId(data["piece"]["_id"]),
+          // "state.color": data["piece"]["color"],
+          // "state.position": {
+          //   row: data["previousPosition"]["row"],
+          //   column: data["previousPosition"]["column"],
+          // },
+        };
+
+        const update = {
+          $set: {
+            "state.$.position": {
+              row: data["piece"]["position"]["row"],
+              column: data["piece"]["position"]["column"],
+            },
+          },
+        };
+
+        console.log('where', JSON.stringify(where, null, 2))
+        console.log('update', JSON.stringify(update, null, 2))
+
+        
+
+        ChessGame.updateOne(where, update, function (err, docs) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Updated Docs : ", docs);
+          }
+        });
+
+      //   ChessGame.findOneAndUpdate(
+      //      {  "gameId": data["gameId"], },
+      //      { $set: { "state.$[elem].position.row" : data["piece"]["position"]["row"], "state.$[elem].position.column" : data["piece"]["position"]["column"] } },
+      //      [ { "elem.name": data["piece"]["name"].toUpperCase(), "elem.color": data["piece"]["color"], "elem.position.row": data["previousPosition"]["row"], "elem.position.column": data["previousPosition"]["column"] } ]
+      //  )
+      }
+    );
+  });
 
   //Whenever someone disconnects this piece of code executed
-  socket.on('disconnect', function () {
-     console.log('A user disconnected');
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
   });
 });
+
+// const a = {
+//    "gameId": "vnfpcxaueh",
+//    "state.name": "PAWN",
+//    "state.color": "WHITE",
+//    "state.position": {
+//      "row": 6,
+//      "column": 3
+//    }
+// }
+
+// const b = {
+//      "$set": {
+//        "state.$.position": {
+//          "row": 5,
+//          "column": 3
+//        }
+//      }
+//    }
+
+//    db.getCollection("chess_games").findAndModify({
+//     query: {  "gameId": "a3tm4m7s5f", },
+//     update: { $set: { "grades.$[elem].row" : 2, "grades.$[elem].column" : 1 } },
+//     arrayFilters: [ { "elem.name": "PAWN", "elem.color": "BLACK", "elem.position.row": 1, "elem.position.column": 1 } ]
+//  })
